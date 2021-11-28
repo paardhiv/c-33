dddbe4c95d3572b2df4ee5b35348a9dd83e8ba97
paardhiv <78153306+paardhiv@users.noreply.github.com>
2021-11-28 18:34:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+var backgroundImg;
+var maxSnow = 100;
+var snow = [];
+var rand;
+
+function preload() {
+  backgroundImg = loadImage("snow1.jpg");
+}
+
+function setup() {
+  engine = Engine.create();
+  world = engine.world;
+  createCanvas(800, 400);
+  //createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  Engine.update(engine);
+  //background(255, 255, 255);
+  background(backgroundImg);
+  rand = Math.round(random(1, 4));
+  if (frameCount % 5 === 0) {
+    snow.push(new Snow(random(0, 800), 30, 30));
+
+  }
+  for(var j = 0; j<snow.length; j++) { snow[j].display() }
+  drawSprites();
+}
